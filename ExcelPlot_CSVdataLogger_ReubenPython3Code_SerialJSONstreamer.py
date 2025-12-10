26298256c0419b9dd11cf2df11d14ca49d13fe91
Reuben Brewer <reuben.brewer@gmail.com>
2025-12-09 17:11:47 -0800

    Add files via upload

--- ExcelPlot_CSVdataLogger_ReubenPython3Code_SerialJSONstreamer.py
+++ ExcelPlot_CSVdataLogger_ReubenPython3Code_SerialJSONstreamer.py
@@ -6,7 +6,7 @@ reuben.brewer@gmail.com
 www.reubotics.com
 
 Apache 2 License
-Software Revision A, 10/27/2024
+Software Revision B, 12/09/2025
 
 Verified working on: Python 3.12 for Windows 11 64-bit and Raspberry Pi Buster (may work on Mac in non-GUI mode, but haven't tested yet).
 '''
